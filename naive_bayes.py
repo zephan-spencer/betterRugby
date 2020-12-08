4bc3f5a6a0deb4d227075465334bb9ca51bb9ad7
Zephan Spencer <number45zephan@gmail.com>
2020-12-08 15:52:43 +0000

    Final Naive Bayes with LDA. Need to explore K means

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,60 +1,90 @@
 from sklearn.datasets import load_iris
+
 from sklearn.model_selection import train_test_split
+
 from sklearn.naive_bayes import GaussianNB
-import pandas as pd
-from pandas import read_csv
+from sklearn.naive_bayes import ComplementNB
+
+from sklearn.cluster import KMeans
+
 from sklearn import preprocessing
+
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+
+from pandas import read_csv
+
+import matplotlib.pyplot as plt
+
+import pandas as pd
 import numpy as np
 import math
 
 pd.options.mode.chained_assignment = None  # default='warn'
 
-names = ['quarter', 'down', 'yardsToGo', 'yardlineNumber',
-# 'offenseFormation',
-# 'personnelO',
-# 'defendersInTheBox',
+names = [
+'quarter',
+'down',
+'yardsToGo',
+'yardlineNumber',
 'numberOfPassRushers',
-# 'personnelD',
-# 'typeDropback',
-'preSnapVisitorScore', 'preSnapHomeScore',
-#'gameClock',
-'absoluteYardlineNumber', 'passResult']
+'preSnapVisitorScore',
+'preSnapHomeScore',
+'absoluteYardlineNumber',
+# 'penaltyCodes'
+# 'offensePlayResult'
+# 'passResult'
+'isDefensivePI'
+]
 
-dataNames = ['quarter', 'down', 'yardsToGo', 'yardlineNumber',
-# 'offenseFormation',
-# 'personnelO',
-# 'defendersInTheBox',
+dataNames = [
+'quarter',
+'down',
+'yardsToGo',
+'yardlineNumber',
 'numberOfPassRushers',
-# 'personnelD',
-# 'typeDropback',
-'preSnapVisitorScore', 'preSnapHomeScore',
-#'gameClock',
-'absoluteYardlineNumber']
+'preSnapVisitorScore',
+'preSnapHomeScore',
+'absoluteYardlineNumber'
+]
 
-resultNames = ['passResult']
+resultNames = ['isDefensivePI']
+
+# resultNames = ['penaltyCodes']
+
+# resultNames = ['offensePlayResult']
+
+# resultNames = ['passResult']
 
 fullDataset = read_csv('Data/plays.csv')
 
 features = fullDataset[names]
 
+# Get rid of any row that doesn't have one of the datapoints we're looking at
 allClear = features.dropna()
 
 finalData = allClear[names]
 
-scaler = preprocessing.StandardScaler()
-
 X = finalData[dataNames]
 Y = finalData[resultNames]
 
-X_Scaled = scaler.fit_transform(X)
+# Dimension Reduction via LDA
+scaler = LDA()
+X = scaler.fit_transform(X,Y.values.ravel())
 
-X_train, X_test, y_train, y_test = train_test_split(X_Scaled, Y, test_size=0.5, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.5, random_state=0)
 
+# gnb = ComplementNB(norm = True)
 gnb = GaussianNB()
 y_pred = gnb.fit(X_train, y_train.values.ravel()).predict(X_test)
-print(y_pred[0])
+
+# print("Class Priors: " + str(gnb.class_prior_[1]))
 
 y_pred = y_pred.reshape(len(y_pred),1)
 
 print("Number of mislabeled points out of a total %d points : %d"
       % (X_test.shape[0], (y_test != y_pred).sum()))
+
+# Y.passResult[Y.passResult == 'I'] = 0
+# Y.passResult[Y.passResult == 'C'] = 1
+# Y.passResult[Y.passResult == 'IN'] = 2
+# Y.passResult[Y.passResult == 'S'] = 3
